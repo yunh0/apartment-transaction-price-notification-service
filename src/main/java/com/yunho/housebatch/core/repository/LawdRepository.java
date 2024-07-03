65b145cf77b430dc48b5d2256c4817a5a305f0e8
YunhoNam <nam100400@naver.com>
2024-07-04 01:13:22 +0900

    Add - 엔티티 구현

---
+++ src/main/java/com/yunho/housebatch/core/repository/LawdRepository.java
@@ -0,0 +1,7 @@
+package com.yunho.housebatch.core.repository;
+
+import com.yunho.housebatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+}
