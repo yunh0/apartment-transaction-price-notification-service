b2880e7fa5b4217caccb574a17c6211365b4b0a3
YunhoNam <nam100400@naver.com>
2024-07-03 22:24:28 +0900

    프로젝트  셋업

---
+++ src/main/java/com/yunho/housebatch/HouseBatchApplication.java
@@ -0,0 +1,13 @@
+package com.yunho.housebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseBatchApplication.class, args);
+    }
+
+}
